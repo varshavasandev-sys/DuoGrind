1c0c2ee4544d2e6937a0239baefe529fab43636e
varshavasandev-sys <varshavasan.dev@gmail.com>
2026-06-20 01:46:17 +0530

    Create color.js

---
+++ app/theme/color.js
@@ -0,0 +1,8 @@
+export const colors = {
+  background: "#0F172A",
+  card: "#1E293B",
+  primaryGradient: ["#6366F1", "#8B5CF6"],
+  secondaryGradient: ["#4F46E5", "#6366F1"],
+  textPrimary: "#F8FAFC",
+  textSecondary: "#94A3B8",
+};
