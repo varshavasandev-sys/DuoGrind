bf7e44580b9ddd9fcf5775e15a92b85723a071d0
varshavasandev-sys <varshavasan.dev@gmail.com>
2026-06-20 01:48:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,31 @@
 
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./app/screens/HomeScreen";
+import { View, Text } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+// Placeholder Session Screen (we'll build next)
+function SessionScreen({ route }) {
+  const { duration } = route.params;
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Session: {duration} mins</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Session" component={SessionScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
